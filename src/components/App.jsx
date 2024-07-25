70cb6cb6b983be414221cae4819f93c6090ec2d9
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-07-25 14:20:57 +0300

    install route and create routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import { Header } from './Header/Header';
+import { Route, Routes } from "react-router-dom";
+import { Header } from "./Header/Header";
 
 export const App = () => {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path='/' element={<h1>Home page</h1>} />
-        <Route path='/about' element={<h1>About page</h1>} />
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="/about" element={<h1>About page</h1>} />
       </Routes>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,15 @@
-import { Link } from 'react-router-dom';
-import s from './Header.module.css';
+import { Link } from "react-router-dom";
+import s from "./Header.module.css";
 export const Header = () => {
   return (
     <header className={s.header}>
-      <div>Routing</div>
       <nav>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to='/about'>About</Link>
+            <Link to="/about">About</Link>
           </li>
         </ul>
       </nav>
